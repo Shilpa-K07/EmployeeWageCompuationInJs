2b9676c056ee20d3d9f9585f29ef87ccedf295de
shilpa <shilpa07udupi@gmail.com>
2020-11-13 20:34:14 +0530

    feat: add parttime employee

--- empWageComputation.js
+++ empWageComputation.js
@@ -1,16 +1,24 @@
 const FULL_TIME = 1;
+const PART_TIME = 2;
 const FULL_DAY_HOUR = 8;
+const PART_TIME_HOUR = 4;
 const WAGE_PER_HOUR = 20;
 
-const randomValue = () =>  Math.floor(Math.random()*10)%2;
+const randomValue = () =>  Math.floor(Math.random()*10)%3;
 const wageCalculation = (hour, wagePerHour) => hour * wagePerHour;
 
 /**
- *  @description Check whether employee is present or not 
+ *  @description Check wheteher employee is present or not 
  */
  var attendenceCheck = function(){
     var randomNum = randomValue();
-    if(randomNum == FULL_TIME ? console.log("\nEmployee is Present  !") : console.log("\nEmployee is absent !"));
+    if(randomNum == FULL_TIME){
+        console.log("\nEmployee is full time  !")
+    }else if(randomNum == PART_TIME){
+        console.log("\nEmployee is part time !");
+    }else{
+        console.log("\nEmployee is absent !")
+    }
     return randomNum; 
 }
 
@@ -23,13 +31,16 @@ var calculateDailyWage = function(randomValue){
     let dailyWage;
     if(randomValue == FULL_TIME){
         workingHour = FULL_DAY_HOUR;
+    }else if(randomValue == PART_TIME){
+        workingHour = PART_TIME_HOUR;
     }else{
         workingHour = 0;
     }
     dailyWage = wageCalculation(workingHour, WAGE_PER_HOUR);
     return dailyWage;
 }
-
 var randomNum = attendenceCheck();
 var dailyWage = calculateDailyWage(randomNum);
 console.log("Daily wage is : "+dailyWage);
+
+
