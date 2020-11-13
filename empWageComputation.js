99b1fcd557a12bf6bffdbf23f8a755223a98298d
shilpa <shilpa07udupi@gmail.com>
2020-11-13 19:51:31 +0530

    feat: add part time employee

--- empWageComputation.js
+++ empWageComputation.js
@@ -1,12 +1,34 @@
 const FULL_TIME = 1;
-var randomValue = () =>  Math.floor(Math.random()*10)%2;
+const FULL_DAY_HOUR = 8;
+const WAGE_PER_HOUR = 20;
 
+const randomValue = () =>  Math.floor(Math.random()*10)%2;
+const wageCalculation = (hour, wagePerHour) => hour * wagePerHour;
 /**
- *  @description Check wheteher employee is present or not 
+ *  @description Check whether employee is present or not 
  */
  var attendenceCheck = function(){
     var randomNum = randomValue();
     if(randomNum == FULL_TIME ? console.log("\nEmployee is Present  !") : console.log("\nEmployee is absent !"));
-    return randomValue; 
+    return randomNum; 
 }
-attendenceCheck();
+
+/**
+ *  @description Daily wage calculation
+ */
+var calculateDailyWage = function(randomValue){
+    
+    let workingHour;
+    let dailyWage;
+    if(randomValue == FULL_TIME){
+        workingHour = FULL_DAY_HOUR;
+    }else{
+        workingHour = 0;
+    }
+    dailyWage = wageCalculation(workingHour, WAGE_PER_HOUR);
+    return dailyWage;
+}
+
+var randomNum = attendenceCheck();
+var dailyWage = calculateDailyWage(randomNum);
+console.log("Daily wage is : "+dailyWage);
