6194b801a8a78d403528ecf396d0fad2f976eb3c
shilpa <shilpa07udupi@gmail.com>
2020-11-13 21:44:11 +0530

    feat: calculate wage till maximum days or hours is reached

--- empWageComputation.js
+++ empWageComputation.js
@@ -4,19 +4,25 @@ const FULL_DAY_HOUR = 8;
 const PART_TIME_HOUR = 4;
 const WAGE_PER_HOUR = 20;
 const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS = 100;
 
 const randomValue = () =>  Math.floor(Math.random()*10)%3;
 const wageCalculation = (hour, wagePerHour) => hour * wagePerHour;
 const totalWageCalculation = (dailyWage, totalWage) => totalWage + dailyWage;
+const totalWorkingHour = (workingHour, currentWorkingHour) => currentWorkingHour + workingHour;
 
 /**
- *  @description Wage calculation for a month
+ *  @description Wage calculation till condition of 
+ * maximum days or maximum hours is reached
  */
-var wageCalculationForMonth = function (){
+
+var wageCalculationForMaximumHourOrAndDay = function (){
     let currentWorkingDay = 0;
     let dailyWage = 0;;
     let totalWage = 0;
-    while(currentWorkingDay <= MAX_WORKING_DAYS){
+    let currentWorkingHour = 0;
+    let workingHour = 0;
+    while(currentWorkingDay <= MAX_WORKING_DAYS || currentWorkingHour <= MAX_WORKING_HOURS){
         var randomNum = randomValue();
         switch(randomNum){
             case FULL_TIME : 
@@ -28,13 +34,16 @@ var wageCalculationForMonth = function (){
             default:
             workingHour = 0;
         }
-        currentWorkingDay ++;
         dailyWage = wageCalculation(workingHour, WAGE_PER_HOUR);
-        totalWage = totalWageCalculation(dailyWage, totalWage); 
+        currentWorkingDay ++;
+        currentWorkingHour = totalWorkingHour(workingHour, currentWorkingHour);
+        if(currentWorkingHour <= 100){
+            totalWage = totalWageCalculation(dailyWage, totalWage);
+        } 
     }
     return totalWage;
 }
 
-var wageForMonth = wageCalculationForMonth();
-console.log("Wage for a month is : "+wageForMonth);
+var wageForMaxDayOrHour = wageCalculationForMaximumHourOrAndDay();
+console.log("Wage calculated for maximum days or hours is : "+wageForMaxDayOrHour);
 
