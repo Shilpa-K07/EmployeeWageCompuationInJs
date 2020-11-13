34c4b01eefbda0b3e0d0fdd0bd7eda6d260f0bee
shilpa <shilpa07udupi@gmail.com>
2020-11-13 18:48:07 +0530

    feat: Employee attendence check

--- empWageComputation.js
+++ empWageComputation.js
@@ -1,1 +1,12 @@
-console.log("Welcome to Employee wage computation !");
+const FULL_TIME = 1;
+var randomValue = () =>  Math.floor(Math.random()*10)%2;
+
+/**
+ *  @description Check wheteher employee is present or not 
+ */
+ var attendenceCheck = function(){
+    var randomNum = randomValue();
+    if(randomNum == FULL_TIME ? console.log("\nEmployee is Present  !") : console.log("\nEmployee is absent !"));
+    return randomValue; 
+}
+attendenceCheck();
